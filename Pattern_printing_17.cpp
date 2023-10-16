fd28af199f4bb82e7c708441af7f5126a6ae5f54
SushruthMurugan <143431420+SushruthMurugan@users.noreply.github.com>
2023-10-16 17:11:14 +0530

    Add files via upload

---
+++ Pattern_printing_17.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,k;
+	k=1;
+	printf("Enter the value of n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<i+1;j++,k++)
+			printf("%d ",k);
+		printf("\n");
+	}
+}
---
+++ Pattern_printing_4.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,s,n;
+	printf("Enter the value of n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(s=0;s<i;s++)
+			printf(" ");
+			{
+				for(j=0;j<n-i;j++)
+					printf("*");
+				printf("\n");
+			}
+	}
+}
+
